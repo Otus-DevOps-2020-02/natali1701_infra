27a9c0549715705dc0cf384dd40be4e69c8f682d
natali1701 <naumenko_nataly@mail.ru>
2020-06-12 12:56:53 -0400

    HW7

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,8 +6,3 @@ variable region {
   # Значение по умолчанию
   default = "europe-west1"
 }
-variable private_key_path {
-  # Описание переменной
-  description = "Path to the private key used for ssh access"
-}
-
