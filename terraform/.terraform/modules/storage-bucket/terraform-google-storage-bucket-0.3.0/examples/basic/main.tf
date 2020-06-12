f940c067b655d6f1d7ebfa46b3463adf61a4dc90
natali1701 <naumenko_nataly@mail.ru>
2020-06-12 12:40:13 -0400

    HW7

--- terraform/.terraform/modules/storage-bucket/terraform-google-storage-bucket-0.3.0/examples/basic/main.tf
+++
@@ -1,6 +0,0 @@
-module "my_awesome_bucket" {
-  source    = "git::https://github.com/SweetOps/terraform-google-storage-bucket.git?ref=master"
-  name      = "bucket"
-  stage     = "production"
-  namespace = "sweetops"
-}
