37be4ec42b8bac8411909fc581cd120b0e5e713e
natali1701 <naumenko_nataly@mail.ru>
2020-06-12 05:52:47 -0400

    HW7

--- terraform/.terraform/modules/storage-bucket/terraform-google-storage-bucket-0.3.0/main.tf
+++ terraform/.terraform/modules/storage-bucket/terraform-google-storage-bucket-0.3.0/main.tf
@@ -1,13 +1,13 @@
 module "label" {
   source      = "git::https://github.com/SweetOps/terraform-null-label.git?ref=tags/0.7.0"
-  enabled     = var.enabled
-  namespace   = var.namespace
-  name        = var.name
-  stage       = var.stage
-  environment = var.environment
-  delimiter   = var.delimiter
-  attributes  = var.attributes
-  tags        = var.tags
+#  enabled     = var.enabled
+#  namespace   = var.namespace
+#  name        = var.name
+#  stage       = var.stage
+#  environment = var.environment
+#  delimiter   = var.delimiter
+#  attributes  = var.attributes
+#  tags        = var.tags
 }
 
 resource "google_storage_bucket" "default" {
